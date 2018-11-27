723079abefebb163c72302b860688e661372325e
waiss67 <baqui.waiss@gmail.com>
2018-11-27 18:58:37 +0100

    update models of url

--- cde.js
+++ cde.js
@@ -15,52 +15,50 @@ let SpinalFile = class SpinalFile extends globalType.Model {
     }
   }
 }
-
-let SpinalNote = class SpinalNote extends Model {
-    constructor(name = "message") {
-      super();
-  
-      this.add_attr({
-        username: "",
-        owner: "",
-        date: Date.now(),
-        message: ""
-      });
-    }
-  };
-
-  let SpinalURL = class SpinalURL extends Model {
-    constructor(name = "url") {
-      super();
-  
-      this.add_attr({
-        name: "",
-        date: Date.now(),
-        url: ""
-      });
-    }
-  };
-
-  var SpinalAttribute = class SpinalAttribute extends Model {
-    constructor(name = "attributes") {
-      super();
-  
-      this.add_attr({
-        name: "",
-        value: "",
-        date: Date.now()
-      });
-    }
-  };
-  
 exports.SpinalFile = SpinalFile;
 spinalCore.register_models(SpinalFile);
+let SpinalNote = class SpinalNote extends Model {
+  constructor(name = "message") {
+    super();
 
+    this.add_attr({
+      username: "",
+      owner: "",
+      date: Date.now(),
+      message: ""
+    });
+  }
+};
 exports.SpinalNote = SpinalNote;
 spinalCore.register_models(SpinalNote);
+let SpinalURL = class SpinalURL extends Model {
+  constructor(name, url) {
+    super();
 
+    this.add_attr({
+      nameURL: name,
+      date: Date.now(),
+      URL: url
+    });
+  }
+};
 exports.SpinalURL = SpinalURL;
 spinalCore.register_models(SpinalURL);
+
+var SpinalAttribute = class SpinalAttribute extends Model {
+  constructor(name = "attributes") {
+    super();
+
+    this.add_attr({
+      name: "",
+      value: "",
+      date: Date.now()
+    });
+  }
+};
+
+
+
 
 exports.SpinalAttribute = SpinalAttribute;
 spinalCore.register_models(SpinalAttribute);
